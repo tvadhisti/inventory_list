9133f7aad86510f6a427d8d6e7d9d1e8f53d08a7
tvadhisti <tiva.adhisti@ui.ac.id>
2023-09-12 23:59:11 +0700

    update model and html

--- main/models.py
+++ main/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-class Product(models.Model):
+class Item(models.Model):
     name = models.CharField(max_length=255)
     amount = models.IntegerField()
     description = models.TextField()
