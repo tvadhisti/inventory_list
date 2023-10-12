2d4a98444d05a6c7a9af8e83634b175e9753af27
tvadhisti <tiva.adhisti@ui.ac.id>
2023-10-12 10:59:13 +0700

    assignment 6

--- main/views.py
+++ main/views.py
@@ -7,17 +7,18 @@ from django.http import HttpResponse
 from django.core import serializers
 from django.shortcuts import redirect
 from django.contrib.auth.forms import UserCreationForm
-from django.contrib import messages  
+from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 import datetime
+from django.views.decorators.csrf import csrf_exempt
+
 
 @login_required(login_url='/login')
 def show_main(request):
     items = Item.objects.filter(user=request.user)
-    count= Item.objects.filter(user=request.user).count()
+    count = Item.objects.filter(user=request.user).count()
     # products = Product.objects.filter(user=request.user)
-
 
     context = {
         'app_name': 'Inventory Management',
@@ -27,10 +28,11 @@ def show_main(request):
         'items': items,
         'items_count': count,
         'last_login': request.COOKIES['last_login'],
-    
+
     }
 
     return render(request, "main.html", context)
+
 
 def create_product(request):
     form = ProductForm(request.POST or None)
@@ -52,12 +54,15 @@ def register(request):
         form = UserCreationForm(request.POST)
         if form.is_valid():
             form.save()
-            messages.success(request, 'Your account has been successfully created!')
+            messages.success(
+                request, 'Your account has been successfully created!')
             return redirect('main:login')
-    context = {'form':form}
+    context = {'form': form}
     return render(request, 'register.html', context)
 
 # login function to authenticate a user
+
+
 def login_user(request):
     if request.method == 'POST':
         username = request.POST.get('username')
@@ -65,13 +70,15 @@ def login_user(request):
         user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
-            response = HttpResponseRedirect(reverse("main:show_main")) 
+            response = HttpResponseRedirect(reverse("main:show_main"))
             response.set_cookie('last_login', str(datetime.datetime.now()))
             return response
         else:
-            messages.info(request, 'Sorry, incorrect username or password. Please try again.')
+            messages.info(
+                request, 'Sorry, incorrect username or password. Please try again.')
     context = {}
     return render(request, 'login.html', context)
+
 
 def logout_user(request):
     logout(request)
@@ -80,21 +87,51 @@ def logout_user(request):
     return response
 
 
-
 def show_xml(request):
     data = Item.objects.all()
     return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
+
 
 def show_json(request):
     data = Item.objects.all()
     return HttpResponse(serializers.serialize("json", data), content_type="application/json")
 
 # Retrieving Data Based on ID in XML and JSON Formats
+
+
 def show_xml_by_id(request, id):
     # store the query result of data with a specific ID from the Product model
     data = Item.objects.filter(pk=id)
     return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
+
+
 def show_json_by_id(request, id):
     # store the query result of data with a specific ID from the Product model
     data = Item.objects.filter(pk=id)
     return HttpResponse(serializers.serialize("json", data), content_type="application/json")
+
+
+def get_product_json(request):
+    product_item = Item.objects.filter(user=request.user)
+    return HttpResponse(serializers.serialize('json', product_item))
+
+
+def count_product_json(request):
+    count = Item.objects.filter(user=request.user).count()
+    return HttpResponse(serializers.serialize('json', count))
+
+
+def add_product_ajax(request):
+    if request.method == 'POST':
+        name = request.POST.get("name")
+        amount = request.POST.get("amount")
+        description = request.POST.get("description")
+        user = request.user
+
+        new_product = Item(name=name, amount=amount,
+                           description=description, user=user)
+        new_product.save()
+
+        return HttpResponse(b"CREATED", status=201)
+
+    return HttpResponseNotFound()
